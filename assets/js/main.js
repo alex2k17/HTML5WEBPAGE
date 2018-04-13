a652ea5d35d3641079307943e1e58dd2c46f80ad
alex2k17 <alex.moya.romero@gmail.com>
2018-04-13 11:39:59 +0200

    Affix apply to all screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -64,6 +59,10 @@
 					skel.breakpoint('medium').active
 				);
 			});
+
+		//$(".navbar").affix({offset: {top: $("#banner").outerHeight(true)} });
+		$( ".navbar" ).attr( "data-offset-top", $("#banner").outerHeight(true) );
+
 
 		// Scrolly.
 			$('.scrolly')
