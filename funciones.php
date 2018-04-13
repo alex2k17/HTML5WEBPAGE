fc3ac6e716ad0bc314669c2cc148fa95990591a1
alex2k17 <alex.moya.romero@gmail.com>
2018-04-13 16:23:30 +0200

    Login

---
+++ funciones.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once ("DATOS/bddConnection.php");
+
+
+if (isset($_POST["accion"]))
+	$accion = $_POST["accion"];
+else 
+	$accion = "";
+
+
+
+if($accion=="login"){
+	
+}
+
+
+
+
+?>
--- login.php
+++ login.php
@@ -7,18 +7,26 @@ require_once("DATOS/bddConnection.php")
 	<head>
 		<title>Login</title>
 		<link rel="stylesheet" type="text/css" href="./css/mainLogin">
-		<script src="mainLogin.js"></script>
+		<script src="js/mScript.js"></script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<main>
 			<section>
 			  <h3>Login</h3>
-			  <form action="./">
+			  <form id="loginform" method="post">
 			    <label for="usuario">Usuario</label>
 			    	<input type="text" id="usuario" name="usuario" placeholder="Introduce tu usuario..">
 			    <label for="password">Contraseña</label>
 			    	<input type="password" id="password" name="password" placeholder="Introduce tu contraseña..">
-			    <input type="button" value="button">
+			    <input type="submit" value="Login" name="loginsub">
 			  </form>
 			</section>
 		</main>
