5b7c1b448a23dde779eaf8ed92c6109fb12efdd5
alex2k17 <alex.moya.romero@gmail.com>
2018-04-16 13:02:06 +0200

    UP

--- funciones.php
+++ funciones.php
@@ -62,6 +62,17 @@ function createUI() {
       	$contenido .= "</div>";
     $contenido .= "</nav>";
 
+    $contenido .= "<div class='sidenav'>
+  <a href='#'>About</a>
+  <a href='#'>Services</a>
+  <a href='#'>Clients</a>
+  <a href='#'>Contact</a>
+</div>
+
+<div class='main'>
+  <h2>Sidenav Example</h2>
+  <p>This sidenav is always shown.</p>
+</div>";
 
 	echo $contenido;
 }
